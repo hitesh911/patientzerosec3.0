7024490d04f43538575038652ea54b6886b7d604
hitesh911 <72139864+hitesh911@users.noreply.github.com>
2022-02-21 20:32:47 +0300

    dad

--- dist/main.js
+++ dist/main.js
@@ -9,19 +9,51 @@ function close_thanks_modal() {
     modal.style.display = "none"
     window.location.reload(true)
 
+
+}
+window.onload = function (){
+    // listner for sidebar-button
+    var sidebar_button = document.getElementById("sidebar-button")
+    sidebar_button.addEventListener("click" ,e =>{
+        var sidebar_content = document.getElementById("sidebar-content")
+        sidebar_content.style.visibility = "visible"
+        sidebar_content.style.transform = "translateX(0%)"
+    })
+    // listner for more_details_button
+    var read_more_button  = document.getElementById('more-details-button')
+    if(read_more_button != null){
+    read_more_button.addEventListener("click",btn =>{
+        // toggling open class 
+        var more_details = document.getElementById("more-details")
+        more_details.classList.toggle("read_more_open")
+        if(read_more_button.innerHTML === "Read More"){
+            read_more_button.innerHTML = "Collapse"
+        }else{
+            read_more_button.innerHTML = "Read More"
+        }
+
+    })
 }
 
-document.onreadystatechange = function(){
-    if(document.readyState != "complete"){
-        const loading_bar = document.getElementById("loading")
-        loading.style.display = "flex"
-
+    
+    
+    document.addEventListener("click" ,e =>{
+        // this is for sidebar close
+        var sidebar = document.getElementById("side-bar")
+        var sidebar_content = document.getElementById("sidebar-content")
+        var isclickonSidebar = sidebar.contains(e.target);
+        var isclickonSidebarContent = sidebar_content.contains(e.target);
+        if(!isclickonSidebar && !isclickonSidebarContent){
+            sidebar_content.style.transform = "translate(100%)"
+        }
+    // services dropdown mobile
+    const is_services_dropdown_menu_mobile = e.target.matches("[data-dropdown-button-mobile]")
+    if(!is_services_dropdown_menu_mobile ) return
+    if(is_services_dropdown_menu_mobile){
+        let that_dropdown = e.target.closest("[data-dropdown-mobile]")
+        that_dropdown.classList.toggle("active-mobile")
     }
-}
-
-// dropdown
-console.log("hello")
-document.addEventListener("click" ,e =>{
+    // dropdown desktop
     const is_services_dropdown_menu = e.target.matches("[data-dropdown-button]")
     if(!is_services_dropdown_menu && e.target.closest('[data-dropdown]') != null) return
     let currentDropdown
@@ -35,4 +67,5 @@ document.addEventListener("click" ,e =>{
         dropdown.classList.remove("active1")
     })
 })
+}
 
